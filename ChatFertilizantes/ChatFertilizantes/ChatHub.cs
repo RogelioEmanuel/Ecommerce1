625091c715321d7190e0cf5a66a9606692d388b0
RogelioEmanuel <94704421+RogelioEmanuel@users.noreply.github.com>
2022-05-17 23:11:54 -0500

    Ecommerce

---
+++ ChatFertilizantes/ChatFertilizantes/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ChatFertilizantes
+{
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void Send(string name, string message)
+        {
+            Clients.All.sendChat(name, message);
+        }
+    }
+}
---
+++ ChatFertilizantes/ChatFertilizantes/Controllers/ComentariosController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ChatFertilizantes.Models;
+
+namespace ChatFertilizantes.Controllers
+{
+    public class ComentariosController : Controller
+    {
+        private FertilizantesModelos db = new FertilizantesModelos();
+
+        // GET: Comentarios
+        public ActionResult Index()
+        {
+            return View(db.Comentarios.ToList());
+        }
+
+        public ActionResult Chat()
+        {
+            return View();
+        }
+
+        // GET: Comentarios/Details/5
+        public ActionResult Details()
+        {
+            // if (id == null)int? id
+            //{
+            //  return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            // }
+            //Comentarios comentarios = db.Comentarios.Find(id);
+            //if (comentarios == null)
+            // {
+            //    return HttpNotFound();
+            //}
+            //return View(comentarios);
+            return View();
+        }
+
+        // GET: Comentarios/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Comentarios/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Idcomentario,comentario,respuesta,status,idproducto,idcliente")] Comentarios comentarios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Comentarios.Add(comentarios);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(comentarios);
+        }
+
+        // GET: Comentarios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comentarios comentarios = db.Comentarios.Find(id);
+            if (comentarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comentarios);
+        }
+
+        // POST: Comentarios/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Respuesta([Bind(Include = "Idcomentario,idcliente,comentario,respuesta,status,idproducto")] Comentarios comentarios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(comentarios).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(comentarios);
+        }
+
+        public ActionResult Respuesta(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comentarios comentarios = db.Comentarios.Find(id);
+            if (comentarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comentarios);
+        }
+
+        // POST: Comentarios/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Idcomentario,comentario,respuesta,status,idproducto,idcliente")] Comentarios comentarios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(comentarios).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(comentarios);
+        }
+
+        // GET: Comentarios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comentarios comentarios = db.Comentarios.Find(id);
+            if (comentarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comentarios);
+        }
+
+        // POST: Comentarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Comentarios comentarios = db.Comentarios.Find(id);
+            db.Comentarios.Remove(comentarios);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+    }
+}
