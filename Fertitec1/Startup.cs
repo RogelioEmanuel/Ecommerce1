b5a4f8f953e6ae491d083131adfe658c8407cb13
Erick-GM25 <102762805+Erick-GM25@users.noreply.github.com>
2022-05-21 15:07:51 -0500

    Cliente Registrado

---
+++ Fertitec1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fertitec.Startup))]
+namespace Fertitec
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
