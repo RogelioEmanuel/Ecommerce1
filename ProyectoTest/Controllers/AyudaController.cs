43b2be4e195a45eebdbb4fd7def6e2bf1a460fcd
Jesús Chávez García <jesus.chavez.garcia@hotmail.com>
2022-05-23 03:00:18 -0500

    Ultima Actualizacion

---
+++ ProyectoTest/Controllers/AyudaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoTest.Controllers
+{
+    public class AyudaController : Controller
+    {
+        // GET: Ayudo
+        public ActionResult ayuda()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProyectoTest/Controllers/ContactoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoTest.Controllers
+{
+    public class ContactoController : Controller
+    {
+        // GET: Contacto
+        public ActionResult contacto()
+        {
+            return View();
+        }
+    }
+}
